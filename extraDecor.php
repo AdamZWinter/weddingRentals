c391345d0c2984a2cae04aa3fdadf6d235c1924d
Kat <katstanford@hotmail.com>
2022-12-04 17:46:32 +0000

    migrating extras to extrasDecor.php

---
+++ extraDecor.php
@@ -0,0 +1,179 @@
+<?php
+require('header.php');
+?>
+
+<section class="section back-img">
+ <h1 class = "site-head">Extras</h1>
+  
+  <center>
+  <p class="top-desc">Choose from our available decor items to complete the style of your event. </p></center>
+  </section>
+ 
+  <section class= "section">
+    <div class="topper">
+  <h2 class="section-head">Hexagon Arbor
+  </h2>
+      <center>
+        <div class=text-box>
+    <p class="description">Handmade; incorporating reclaimed wood, & custom laser cut metal brackets.     
+Price includes delivery, set-up and tear-down within a 30-mile radius of Orrville, OH <br> $350</p>    
+          
+        </div>
+      </center>
+    </div>
+
+    
+    <div class="slideshow-container">
+
+<div class="slider">
+  <div class="slide">    
+    <img class= "slide-pics rounded" src="img/HexArch2.jpg" alt="wedding arch" >
+  </div>
+
+  <div class="slide">   
+    <img class= "slide-pics rounded" src="img/HexArch1.jpg" alt="wedding arch" >
+  </div>
+
+  <div class="slide">
+       <img class= "slide-pics rounded" src="img/HexArch3.jpg" alt= "wedding arch">
+  </div>
+  <div class="slide">
+       <img class= "slide-pics rounded" src="img/HexArch4.jpg" alt= "wedding arch">
+  </div>
+  <div class="slide">
+       <img class= "slide-pics rounded" src="img/HexArch5.jpg" alt= "wedding arch">
+  </div>
+  <div class="slide">
+       <img class= "slide-pics rounded" src="img/HexArch6.jpg" alt="wedding arch">
+  </div>
+
+  <!-- Next and previous buttons/dots below slider -->
+        <button class="slider_btn slider_btn-left">&larr;</button>
+        <button class="slider_btn slider_btn-right">&rarr;</button>
+        <div class="dots1"></div>
+ 
+</div>      
+    </div>
+    
+  </section>
+
+<!--   SECOND SLIDESHOW -->
+   <section class= "section">
+    
+  <h2 class="section-head">Decor Accents</h2>
+      <center>
+        <div class=text-box>
+    <p class="description">Vintage sofa, convenient containers, and one-of-a-kind extras to streamline & complete your look. <br></p>    
+          
+        </div>
+      </center>
+   
+
+    
+    <div class="slideshow-container">
+
+<div class="slider2">
+  <div class="slide2">    
+    <img class= "slide-pics rounded" src="img/DecorAccent1.jpg" alt="Couple on vintage sofa">
+  </div>
+
+  <div class="slide2">   
+    <img class= "slide-pics rounded" src="img/DecorAccent2.jpg" alt="vintage sofa">
+  </div>
+
+  <div class="slide2">
+       <img class= "slide-pics rounded" src="img/DecorAccent3.jpg" alt="Group photo on vintage sofa">
+  </div>
+  <div class="slide2">
+       <img class= "slide-pics rounded" src="img/DecorAccent4.jpg" alt="Antique gallon jugs">
+  </div>
+  <div class="slide2">
+       <img class= "slide-pics rounded" src="img/DecorAccent5.jpg" alt="Antique gallon jugs">
+  </div>
+  <div class="slide2">
+       <img class= "slide-pics rounded" src="img/DecorAccent6.jpg" alt="Ball Jars">
+  </div>
+   <div class="slide2">
+       <img class= "slide-pics rounded" src="img/DecorAccent7.jpg" alt="Blue Jars">
+  </div>
+   <div class="slide2">
+       <img class= "slide-pics rounded" src="img/DecorAccent8.jpg" alt="vintage typewriter">
+  </div>
+   <div class="slide2">
+       <img class= "slide-pics rounded" src="img/DecorAccent9.jpg" alt="vintage candle holder">
+  </div>
+
+  <!-- Next and previous buttons/dots below slider -->
+        <button class="slider_btn2 slider_btn-left secondLeft">&larr;</button>
+        <button class="slider_btn2 slider_btn-right secondRight">&rarr;</button>
+        <div class="dots2"></div>
+</div>
+ 
+</div>   
+      
+
+        <center><a class="btn btn-primary button" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+    Decor Pricing
+  </a></center>
+<div class="collapse" id="collapseExample">
+  <div class="card card-body">
+    <center><p class= "description">
+      DECOR ACCENT PRICING<br>
+
+(Decor pieces are available to rental customers who have contracted a rental set or minimum order for their event date.)<br></p></center>
+    <center><p class="description">
+Vintage Couch - $99 add on to any Delivered Rental<br>
+
+Antique Gallon Jugs - $4 each, 12 available<br>
+
+XL Wine Jugs - $20 each, 3 available<br>
+
+Clear Antique Ball Jars - $30 for 50 Jars (Assorted Sizes)<br>
+
+Blue Antique Ball Jars - $30 for 25 Jars (Assorted Sizes)<br>
+    </p></center>
+    </div>
+  </section>
+
+
+<br>
+  <h2 class= "section-head">Party Pack Rentals</h2>
+  <center>
+    <p class="description">Luxury signage for bridal showers, baby showers, birthday parties, anniversaries, graduation parties, and celebrations of life. <br> Package Rentals starting at $99.
+
+</p>
+  </center>
+  <br>
+  <div class="container-fluid">
+  <div class= "row">
+    <div class="col-md-3">
+      <img src="img/PartyPack3.jpg" class= "img-fluid rounded" alt="Bridal shower sign">
+    </div>
+    <div class="col-md-3">
+      <img src="img/PartyPack2.jpg" class= "img-fluid rounded" alt="wedding sign">
+    </div>
+  
+    
+    <div class="col-md-3">
+      <img src="img/PartyPack1.jpg" class= "img-fluid  rounded" alt="baby shower sign">
+    </div>
+    <div class="col-md-3">
+      <img src="img/PartyPack4.jif" class= "img-fluid rounded" alt="engagement sign">
+    </div>
+    </div>
+  
+  </div>
+  <br>
+  <br>
+  <center><a class="btn btn-primary button"  href="availability.html">
+    Check Availability
+  </a></center>
+  
+<br>
+
+
+
+<?php
+//footer
+require('footer.php');
+?>
